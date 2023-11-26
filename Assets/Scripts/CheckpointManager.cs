e25debd5e7b823b4a3780852e30b9ff87c904c1f
KAY <yalchinkamran@gmail.com>
2023-11-26 03:47:07 +0000

    V6.5
    Need to link both checkpoints and the timer together

---
+++ Assets/Scripts/CheckpointManager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class CheckpointManager : MonoBehaviour
+{
+    public List<CarLapCounter> CarLapCounters = new List<CarLapCounter>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Get Car lap count in the scene
+        CarLapCounter[] carLapCounterArray = FindObjectsOfType<CarLapCounter>();
+
+        // Store the lap count in a list
+        CarLapCounters = carLapCounterArray.ToList<CarLapCounter>();
+
+        // Hook up the passed checkpoint event
+        foreach (CarLapCounter LapCounter in CarLapCounters)
+            LapCounter.OnPassCheckpoint += OnPassCheckpoint;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Timer : MonoBehaviour
+{
+    public Text timerText;
+    static float timer;
+
+    public Text lapText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        int minutes = Mathf.FloorToInt(timer / 60);
+        int seconds = Mathf.FloorToInt(timer - minutes * 60);
+
+        string time = string.Format("{0:0}:{1:00}", minutes, seconds);
+
+        timerText.text = time;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        lapText.text = "Lap: " + timerText.text;
+        timer = 0.0f;
+    }
+}
